5028ba860db16d2209a606b62e37794c1826b58b
Jinliang Yang <yangjl0930@gmail.com>
2016-02-17 17:14:08 -0800

    start to run with tiled data

---
+++ profiling/2.Dm_tile/2.A.1_get_data_4Dm.R
@@ -0,0 +1,7 @@
+### Jinliang Yang
+### Feb 17th, 2016
+
+
+cg1 <- read.table("/group/jrigrp4/BS_teo20/SP028-029_JR_100bp_CG.tab", header=TRUE)
+cg2 <- read.table("/group/jrigrp4/BS_teo20/SP029_2-3_JR_100bp_CG.tab", header=TRUE)
+
