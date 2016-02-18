88b0f758ef40122c7cc2c09a99bc79320e55b7ac
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-02-17 22:47:15 -0800

    updated.

--- profiling/2.Dm_tile/2.A.1_get_data_4Dm.R
+++ profiling/2.Dm_tile/2.A.1_get_data_4Dm.R
@@ -2,6 +2,16 @@
 ### Feb 17th, 2016
 
 
-cg1 <- read.table("/group/jrigrp4/BS_teo20/SP028-029_JR_100bp_CG.tab", header=TRUE)
-cg2 <- read.table("/group/jrigrp4/BS_teo20/SP029_2-3_JR_100bp_CG.tab", header=TRUE)
+library("data.table", lib="~/bin/Rlib")
+
+cg1 <- fread("/group/jrigrp4/BS_teo20/SP028-029_JR_100bp_CG.tab", header=TRUE)
+cg2 <- fread("/group/jrigrp4/BS_teo20/SP029_2-3_JR_100bp_CG.tab", header=TRUE)
+
+cg1 <- as.data.frame(cg1)
+cg2 <- as.data.frame(cg2)
+
+
+
+
+
 
