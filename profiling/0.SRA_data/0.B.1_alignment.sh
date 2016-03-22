b4e6f450b67c86c604d351fffb1e3626f0f46dd5
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-03-21 21:19:59 -0700

    updates

--- profiling/0.SRA_data/0.B.1_alignment.sh
+++ profiling/0.SRA_data/0.B.1_alignment.sh
@@ -12,10 +12,16 @@ bismark_genome_preparation --bowtie2 /home/jolyang/dbcenter/AGP/AGPv2/
 
 #(II) Running bismark 
 
-bismark --bowtie2 -n 1 -l 50 /data/genomes/homo_sapiens/GRCh37/ test_dataset.fastq
+#bismark --bowtie2 -n 1 -l 50 /data/genomes/homo_sapiens/GRCh37/ test_dataset.fastq
 
 
-bismark --bowtie1 -n 1 /home/jolyang/dbcenter/AGP/AGPv2 -1 test_1.fastq -2 test_2.fastq
+bismark --bowtie2 -n 1 /home/jolyang/dbcenter/AGP/AGPv2 -p 2 -1 test_1.fastq -2 test_2.fastq --basename test
 
 #(III) Running the Bismark bismark_methylation_extractor
-bismark_methylation_extractor -s --comprehensive test_dataset.fastq_bismark.sam
+bismark_methylation_extractor -s --comprehensive test_1.fastq_bismark_bt2_pe.bam
+
+bismark_methylation_extractor -s --bedGraph --counts --buffer_size 10G test_1.fastq_bismark_bt2_pe.bam
+
+bismark_methylation_extractor -s --bedGraph --counts --buffer_size 10G --cytosine_report --genome_folder /home/jolyang/dbcenter/AGP/AGPv2 test_1.fastq_bismark_bt2_pe.bam
+
+
--- profiling/0.SRA_data/0.B.2_alignment.R
+++ profiling/0.SRA_data/0.B.2_alignment.R
@@ -1,24 +1,14 @@
 ### Jinliang Yang
 ### March 1st, 2016
 
-source("~/Documents/Github/zmSNPtools/Rcodes/set_arrayjob.R")
-run_bs2vcf <- function(indir="/group/jrigrp4/BS_teo20/BSMAP_output"){
-    
-    files=list.files(path=indir, pattern="txt$")
-    for(i in 1:length(files)){
-        outfile <- gsub("txt", "vcf", files[i])
-        
-        shid <- paste0("slurm-script/run_bs2vcf_", i, ".sh")
-        #out <- gsub(".*/", "", out)
-        #outfile <- paste0(outdir, "/", out)
-        cmd <- paste("bs2vcf -p", indir,  "-i", files[i], "-o", outfile)
-        
-        cat(cmd, file=shid, sep="\n", append=FALSE)
-    }
-    
-    set_arrayjob(shid="slurm-script/run_array_bs2vcf.sh",
-                 shcode=paste0("sh slurm-script/run_bs2vcf_$SLURM_ARRAY_TASK_ID.sh"),
-                 arrayjobs="1-12",
-                 wd=NULL, jobid="bs2vcf", email="yangjl0930@gmail.com")
-    
-}
+library(maizeR)
+
+fq1 =list.files(path="/group/jrigrp4/BS_teo20/WGBS", pattern="1.fastq$")
+fq2 =list.files(path="/group/jrigrp4/BS_teo20/WGBS", pattern="2.fastq$")
+
+input_df <- data.frame(fq1=fq1, fq2=fq2)
+input_df$out <- gsub("\\..*", "", input_df$fq1)
+
+runa_bismark(input_df[-6,], genome="/home/jolyang/dbcenter/AGP/AGPv2",
+             cpu=6, outdir="/group/jrigrp4/BS_teo20/WGBS/BSM", arrayjobs="1-5",
+             jobid="bs1-5", email="yangjl0930@gmail.com")
