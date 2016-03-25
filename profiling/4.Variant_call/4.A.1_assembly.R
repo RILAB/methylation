66bc45c8437415dc056d2eefb22b40b53a6d3155
Jinliang Yang <yangjl0930@gmail.com>
2016-03-25 16:00:06 -0700

    updates

--- profiling/4.Variant_call/4.A.1_assembly.R
+++ profiling/4.Variant_call/4.A.1_assembly.R
@@ -1,21 +1,23 @@
 ### Jinliang Yang
 ### use Li Heng's De novo assembly based variant calling pipeline
-
-f1 <- list.files(path="/group/jrigrp/teosinte-parents/seq-merged", pattern="1.fastq.gz", full.names=TRUE)
-f2 <- list.files(path="/group/jrigrp/teosinte-parents/seq-merged", pattern="2.fastq.gz", full.names=TRUE)
-fq <- data.frame(fq1=f1, fq2=f2, out="/home/jolyang/Documents/Github/methylation/largedata/denovocall")
-
-a1 <- gsub("_1.fastq.gz", "", fq$fq1)
-a2 <- gsub("_2.fastq.gz", "", fq$fq2)
-sum(a1 != a2)
+### testing
 
 
-fq$out <- paste(fq$out, gsub(".*Sample_|_index.*", "", fq$fq1), sep="/")
+fq1 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
+fq2 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
+sampleid <- read.table("~/dbcenter/BMfastq/sampleid.txt", header=TRUE)
+
+fq <- data.frame(fq1=fq1[c(1,17)], fq2=fq2[c(1,17)], out=gsub(".sra.*", "", fq1[c(1,17)]),
+                 sample=c("Mo17", "B73"))
+fq$out <- gsub("BMfastq/", "BMfastq/fermi/", fq$out)
+
 
 library(farmeR)
-run_fermikit(fq, kitpath="/home/jolyang/bin/fermikit/fermi.kit",
-             s='2.5g', t=16, l=100, arrayjobs="1",
-             jobid="fermi", email="yangjl0930@gmail.com")
+run_fermikit(fq,
+             kitpath="$HOME/bin/fermikit/fermi.kit",
+             s='2.3g', l=75,
+             email="yangjl0930@gmail.com", 
+             runinfo = c(TRUE, "bigmemh", 16))
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
 ###>>> RUN: sbatch -p bigmemh --mem=128000 --ntasks=16 slurm-script/run_fermikit_array.sh
--- profiling/4.Variant_call/4.A.3_map_variant_call.R
+++ profiling/4.Variant_call/4.A.3_map_variant_call.R
@@ -23,5 +23,38 @@ run_GATK(inputdf, ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.topleve
          email="yangjl0930@gmail.com",
          runinfo = c(FALSE, "bigmemh", 4))
 
+system("sbatch -p bigmemm --mem 32784 --ntasks=4 slurm-script/run_gatk_array.sh")
+
+
+#######################
+bam <- list.files(path="~/dbcenter/BMfastq/bam", pattern="bam$", full.names = TRUE)
+inputdf <- data.frame(bam=bam, out=gsub(".dedup.*", "", bam),
+                      group=c("g1", "g2"), sample=c("Mo17", "B73"), PL="illumina", 
+                      LB=c("lib1", "lib2"), PU=c("unit1", "unit2"))
+
+run_GATK(inputdf, 
+         ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
+         gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
+         picardpwd="$HOME/bin/picard-tools-2.1.1/picard.jar",
+         markDup=FALSE, 
+         realignInDels=FALSE, indels.vcf="indels.vcf",
+         recalBases=FALSE, dbsnp.vcf="dbsnp.vcf", 
+         email="yangjl0930@gmail.com",
+         runinfo = c(FALSE, "bigmemh", 4))
 system("sbatch -p bigmemh --mem 32784 --ntasks=4 slurm-script/run_gatk_array.sh")
+
+
+####### joint variant calling
+gvcf <- list.files(path="~/dbcenter/BMfastq/bam", pattern="vcf$", full.names = TRUE)
+outvcf <- "~/dbcenter/BMfastq/bam/jointbm"
+run_GATK_JointGenotype(
+    gvcf,
+    outvcf,
+    ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
+    gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
+    snpflt="\"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0\"",
+    indelflt="\"QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0\"",
+    email="yangjl0930@gmail.com",
+    runinfo = c(TRUE, "bigmemh", 4) )
+
 
