469dfe9755113ad9bb166178b3a3b2f069fa4e0d
Jinliang Yang <yangjl0930@gmail.com>
2016-03-24 17:58:29 -0700

    updates

---
+++ profiling/4.Variant_call/4.A.3_map_variant_call.R
@@ -0,0 +1,27 @@
+### Jinliang Yang
+### use Li Heng's De novo assembly based variant calling pipeline
+### 3/22/2016
+
+fq1 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
+fq2 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
+
+sampleid <- read.table("~/dbcenter/BMfastq/sampleid.txt", header=TRUE)
+
+inputdf <- data.frame(fq1=fq1[c(1,17)], fq2=fq2[c(1,17)], out="mysample",
+                      group=c("g1", "g2"), sample=c("Mo17", "B73"), PL="illumina", 
+                      LB=c("lib1", "lib2"), PU=c("unit1", "unit2"))
+inputdf$out <- gsub(".sra.*", "", inputdf$fq1)
+
+
+
+
+run_GATK(inputdf, ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
+         gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
+         picardpwd="$HOME/bin/picard-tools-2.1.1/picard.jar",
+         markDup=TRUE, realignInDels=FALSE, indels.vcf="indels.vcf",
+         recalBases=FALSE, dbsnp.vcf="dbsnp.vcf", 
+         email="yangjl0930@gmail.com",
+         runinfo = c(FALSE, "bigmemh", 4))
+
+system("sbatch -p bigmemh --mem 32784 --ntasks=4 slurm-script/run_gatk_array.sh")
+
