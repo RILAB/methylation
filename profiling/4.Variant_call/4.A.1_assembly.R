3dfe5058947d2ee2a037a3c1c89c1220235754ec
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-03-30 12:08:53 -0700

    updates

--- profiling/4.Variant_call/4.A.1_assembly.R
+++ profiling/4.Variant_call/4.A.1_assembly.R
@@ -2,7 +2,7 @@
 ### use Li Heng's De novo assembly based variant calling pipeline
 ### testing
 
-
+library(farmeR)
 fq1 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
 fq2 <- list.files(path="~/dbcenter/BMfastq", pattern="_2.fastq.gz$", full.names = TRUE)
 sampleid <- read.table("~/dbcenter/BMfastq/sampleid.txt", header=TRUE)
@@ -11,12 +11,12 @@ fq <- data.frame(fq1=fq1[c(1,17)], fq2=fq2[c(1,17)], out=gsub(".sra.*", "", fq1[
                  sample=c("Mo17", "B73"))
 fq$out <- gsub("BMfastq/", "BMfastq/fermi/", fq$out)
 
-library(farmeR)
+
 run_fermikit(fq,kitpath="$HOME/bin/fermikit/fermi.kit",
              ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
              s='2.3g', l=75,
              email="yangjl0930@gmail.com", 
-             runinfo = c(TRUE, "bigmemm", 16))
+             runinfo = c(TRUE, "bigmemh", 16))
 
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
