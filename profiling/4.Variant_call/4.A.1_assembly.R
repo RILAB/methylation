a4915d525ef5ccdebcc5728d2bc75042343a9b61
Jinliang Yang <yangjl0930@gmail.com>
2016-03-29 18:14:05 -0700

    updates

--- profiling/4.Variant_call/4.A.1_assembly.R
+++ profiling/4.Variant_call/4.A.1_assembly.R
@@ -4,20 +4,24 @@
 
 
 fq1 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
-fq2 <- list.files(path="~/dbcenter/BMfastq", pattern="_1.fastq.gz$", full.names = TRUE)
+fq2 <- list.files(path="~/dbcenter/BMfastq", pattern="_2.fastq.gz$", full.names = TRUE)
 sampleid <- read.table("~/dbcenter/BMfastq/sampleid.txt", header=TRUE)
 
 fq <- data.frame(fq1=fq1[c(1,17)], fq2=fq2[c(1,17)], out=gsub(".sra.*", "", fq1[c(1,17)]),
                  sample=c("Mo17", "B73"))
 fq$out <- gsub("BMfastq/", "BMfastq/fermi/", fq$out)
 
-
 library(farmeR)
-run_fermikit(fq,
-             kitpath="$HOME/bin/fermikit/fermi.kit",
+run_fermikit(fq,kitpath="$HOME/bin/fermikit/fermi.kit",
+             ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
              s='2.3g', l=75,
              email="yangjl0930@gmail.com", 
-             runinfo = c(TRUE, "bigmemh", 16))
+             runinfo = c(TRUE, "bigmemm", 16))
+
 
 ###>>> In this path: cd /home/jolyang/Documents/Github/methylation
 ###>>> RUN: sbatch -p bigmemh --mem=128000 --ntasks=16 slurm-script/run_fermikit_array.sh
+
+run_fermikit_vcfcall(bamdir="$HOME/dbcenter/BMfastq/fermi", kitpath="$HOME/bin/fermikit",
+                     ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
+                     email="yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemh", 1))
