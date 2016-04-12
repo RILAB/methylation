a645042227dedf2587cb33fdfae1cc487e03d316
Jinliang Yang <yangjl0930@gmail.com>
2016-04-11 17:28:33 -0700

    updates

--- profiling/1.data_description/1.B.2_meth_GERP.R
+++ profiling/1.data_description/1.B.2_meth_GERP.R
@@ -4,13 +4,14 @@
 library("data.table", lib="~/bin/Rlib")
 
 gerp <- fread("~/Documents/Github/pvpDiallel/largedata/GERPv2/gerp130m.csv", sep=",")
+gerp[, range(RS)] #-4.56  2.28
+gerp <- gerp[RS > 0,]
 
 #DT[i, j, by]
 ##   R:      i                 j        by
 ## SQL:  where   select | update  group by
 gerp[, snpid := paste(chr, pos, sep="_")]
 
-gerp[, range(RS)]
 
 
 
