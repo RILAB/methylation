98ea586cdfa3062fd53a10b3a386cd550c102fbf
Jinliang Yang <yangjl0930@gmail.com>
2016-04-20 17:28:26 -0700

    updates

--- profiling/2.Dm_tile/2.B.6_Dm_region_plot.R
+++ profiling/2.Dm_tile/2.B.6_Dm_region_plot.R
@@ -6,27 +6,41 @@ df$geneid <- gsub(".*/|_cg.out", "", df$geneid)
 
 df$pos <- as.numeric(as.character(gsub(".*_", "", df$geneid)))
 
+hist(df$Dm, breaks=100, col="darkblue")
+abline(v= quantile(df$Dm, 0.05), col="red", lwd=2)
+#abline(v= 0, col="grey", lwd=2)
 
-pdf(file="graphs/tem.pdf", height=4, width =10)
-for(i in 1:10){
-    chr <- subset(df, chr==i)
-    chr <- chr[order(chr$pos),]
-    plot(chr$pos, chr$Dm, type="l", main=paste("Chr", i))
-    abline(h = 0, lty=2, lwd=3, col="grey")
-}
+df$d <- df$Dm*2
+quantile(df$d, 0.01)
+
+pdf(file="graphs/region_CG.pdf", width=12, height=5)
+df2 <- subset(df, d < quantile(df$d, 0.05))
+plot_stack(df, df2, chrlen="data/ZmB73_RefGen_v2.length", cex=0.3,
+           cent="data/AGPv2_centromere.csv", plotcol="d", dorescale=FALSE,
+           cols=list("#8b0000", rep(c("slateblue", "cyan4"), 5), "#ff7373"))
+dev.off()
+    
+
+#############
+df <- read.csv("cache/res_region_chg.csv")
+df$geneid <- gsub(".*/|_chg.out", "", df$geneid)
+df$pos <- as.numeric(as.character(gsub(".*_", "", df$geneid)))
+
+hist(df$Dm, breaks=100, col="darkblue")
+abline(v= quantile(df$Dm, 0.05), col="red", lwd=2)
+#abline(v= 0, col="grey", lwd=2)
+
+df$d <- df$Dm*2
+quantile(df$d, 0.01)
+
+pdf(file="graphs/region_CHG.pdf", width=12, height=5)
+df2 <- subset(df, d < quantile(df$d, 0.025) | d > quantile(df$d, 0.975))
+plot_stack(df, df2, chrlen="data/ZmB73_RefGen_v2.length", cex=0.3,
+           cent="data/AGPv2_centromere.csv", plotcol="d", dorescale=FALSE,
+           cols=list("#8b0000", rep(c("slateblue", "cyan4"), 5), "#ff7373"))
 dev.off()
 
-df2 <- read.csv("cache/res_region_chg.csv")
-df2$geneid <- gsub(".*/|_chg.out", "", df2$geneid)
-
-df2$pos <- as.numeric(as.character(gsub(".*_", "", df2$geneid)))
-
-chr <- subset(df2, chr==10)
-chr <- chr[order(chr$pos),]
-plot(chr$pos, chr$Dm, type="l")
-abline(h = 0, lty=2, lwd=3, col="grey")
-
-
+############
 df3 <- read.csv("cache/res_region_chh.csv")
 df3$geneid <- gsub(".*/|_chh.out", "", df3$geneid)
 
