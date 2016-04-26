7e16590a2242f8b22d1d2c943b37bc7ba480b5ac
Jinliang Yang <yangjl0930@gmail.com>
2016-04-26 12:26:02 -0700

    updates

---
+++ profiling/4.Variant_call/4.B.3_GATK_teo20_joint.R
@@ -0,0 +1,22 @@
+
+
+####### joint variant calling
+gvcf <- list.files(path="~/dbcenter/BMfastq/bam", pattern="g.vcf$", full.names = TRUE)
+outvcf <- "~/dbcenter/BMfastq/bam/joint_call.vcf"
+run_GATK_JointGenotype(
+    gvcf, outvcf,
+    ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
+    gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
+    includeNonVariantSites = FALSE,
+    hardfilter= TRUE,
+    snpflt="\"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0\"",
+    indelflt="\"QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0\"",
+    email="yangjl0930@gmail.com",
+    runinfo = c(TRUE, "bigmemh", 4)
+)
+
+#WARN  23:53:16,436 Interpreter - ![38,47]: 'QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0;' undefined variable MQRankSum 
+#WARN  23:55:50,098 Interpreter - ![26,40]: 'QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0;' undefined variable ReadPosRankSum 
+
+
+
