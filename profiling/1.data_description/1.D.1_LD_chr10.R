9ae228ee3c3beca86efe92be13440e11866f32b5
Jinliang Yang <yangjl0930@gmail.com>
2016-04-28 14:14:37 -0700

    updates

---
+++ profiling/1.data_description/1.D.1_LD_chr10.R
@@ -0,0 +1,11 @@
+### Jinliang Yang
+### April 28th, 2016
+### Happy B-day JJ!
+
+
+
+"module load plink/1.90"
+
+"plink -vcf teo20_cg_methratio.vcf.gz --r2 --threads 8 --memory 64000 --out teo20_cg --allow-extra-chr"
+
+
