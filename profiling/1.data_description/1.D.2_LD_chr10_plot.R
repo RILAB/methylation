0dc6789eccfba95d2aa5edaf452c4027cb5e3d43
Jinliang Yang <yangjl0930@gmail.com>
2016-05-02 10:18:19 -0700

    updates

--- profiling/1.data_description/1.D.2_LD_chr10_plot.R
+++ profiling/1.data_description/1.D.2_LD_chr10_plot.R
@@ -4,20 +4,54 @@
 
 
 library("data.table")
-ld <- fread("largedata/vcf_files/teo20_cg.ld")
+get_ld <- function(ldfile="largedata/vcf_files/teo20_chg_10m.ld", binsize=10,
+                   outfile="largedata/ld_chr10_1m_10bp_meanr2.csv"){
+    ld <- fread(ldfile)
+    # 232787512
+    ld[, dis := round((BP_B - BP_A)/binsize, 0)]
+    res <- ld[,.(mr2 = mean(R2)), by=.(dis)] 
+    write.table(res, outfile, sep=",", row.names=FALSE, quote=FALSE)
+    return(ld)
+}
 
 
-chr2 <- ld[CHR_A ==2 ]
+###############
 
-ld[, dis := round((BP_B - BP_A)/1000, 0)]
+res1 <- get_ld(ldfile="largedata/vcf_files/teo20_cg_10m.ld", binsize=10,
+               outfile="largedata/cg_ld_10m_10bp_meanr2.csv")
+
+res2 <- get_ld(ldfile="largedata/vcf_files/teo20_chg_10m.ld", binsize=10,
+               outfile="largedata/chg_ld_10m_10bp_meanr2.csv")
+
+res3 <- get_ld(ldfile="largedata/vcf_files/teo20_chh_10m.ld", binsize=10,
+               outfile="largedata/chh_ld_10m_10bp_meanr2.csv")
 
 
 
-chr2[, dis := round((BP_B - BP_A)/10, 0)]
 
-chr2[, dis := round((BP_B - BP_A)/10, 0)]
 
-sum10bp <- chr2[,.(mr2 = mean(R2)), by=.(dis)] 
-write.table(sum10bp, "cache/ld_chr2_sum10bp.csv", sep=",", row.names=FALSE, quote=FALSE)
 
+d1 <- read.csv("largedata/cg_ld_10m_10bp_meanr2.csv")
+d1 <- d1[order(d1$dis),]
+d1 <- d1[1:50,]
+
+d2 <- read.csv("largedata/chg_ld_10m_10bp_meanr2.csv")
+d2 <- d2[order(d2$dis),]
+d2 <- d2[1:300,]
+
+d3 <- read.csv("largedata/chh_ld_10m_10bp_meanr2.csv")
+d3 <- d3[order(d3$dis),]
+d3 <- d3[1:300,]
+
+plot(d1[-1,]$dis*10, d1[-1,]$mr2, type="l", main="DNA methylation LD decay in teosinte",
+     xlab="Physical Distance (bp)", ylab="LD (r^2)", ylim=c(0.1, 0.22), lwd=3)
+lines(d2[-1,]$dis*10, d2[-1,]$mr2, col="blue", lwd=3)
+lines(d3[-1,]$dis*10, d3[-1,]$mr2, col="red", lwd=3)
+legend("topright", legend=c("CG-SMP", "CHG-SMP", "CHH-SMP"), col=c("black", "blue", "red"), lwd=3)
+
+plot(d2[-1,]$dis*10, d2[-1,]$mr2, col="blue", lwd=3, type="l", main="CHG",
+     xlab="Physical Distance (bp)", ylab="LD (r^2)")
+
+plot(d1[-1,]$dis*10, d1[-1,]$mr2, col="blue", lwd=3, type="l", main="CG",
+     xlab="Physical Distance (bp)", ylab="LD (r^2)")
 
