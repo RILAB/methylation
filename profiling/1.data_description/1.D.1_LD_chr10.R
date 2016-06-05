b64fada0da3b940fc71eecb24ec08b479b827062
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-06-05 11:55:10 -0700

    updates

--- profiling/1.data_description/1.D.1_LD_chr10.R
+++ profiling/1.data_description/1.D.1_LD_chr10.R
@@ -3,12 +3,18 @@
 ### Happy B-day JJ!
 
 
+library("data.table")
+
 
 "module load plink/1.90"
 
 "plink -vcf teo20_cg_methratio.vcf.gz --r2 --threads 8 --memory 64000 --out teo20_cg --allow-extra-chr"
 
 
-
+library("farmeR")
+sh1 <- "sh data/iget.sh"
+set_farm_job(slurmsh = "slurm-script/run_fq.sh",
+             shcode = sh1, wd = NULL, jobid = "fastq",
+             email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemh", "1"))
 
 
