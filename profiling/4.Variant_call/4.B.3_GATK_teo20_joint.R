969c4eef1ca6b653634da4a17d4edb952bca0cc5
Jinliang Yang <yangjl0930@gmail.com>
2016-06-07 17:51:14 -0700

    4 files.

--- profiling/4.Variant_call/4.B.3_GATK_teo20_joint.R
+++ profiling/4.Variant_call/4.B.3_GATK_teo20_joint.R
@@ -4,8 +4,9 @@
 library("farmeR")
 ####### joint variant calling
 gvcf <- list.files(path="largedata/gatk_vcf", pattern="g.vcf$", full.names = TRUE)
-gvcf <- gvcf[1]
-outvcf <- "largedata/gatk_vcf/JRIAL1A_joint_call.vcf"
+#gvcf <- gvcf[1]
+length(gvcf) #18
+outvcf <- "largedata/gatk_vcf/JRI18_joint_call.vcf"
 
 run_GATK_JointGenotype(
     gvcf, outvcf,
@@ -16,18 +17,10 @@ run_GATK_JointGenotype(
     snpflt="\"QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0\"",
     indelflt="\"QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0\"",
     email="yangjl0930@gmail.com",
-    runinfo = c(TRUE, "bigmemh", 8)
+    runinfo = c(TRUE, "bigmemm", 16)
 )
 
 #WARN  23:53:16,436 Interpreter - ![38,47]: 'QD < 2.0 || FS > 60.0 || MQ < 40.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0;' undefined variable MQRankSum 
 #WARN  23:55:50,098 Interpreter - ![26,40]: 'QD < 2.0 || FS > 200.0 || ReadPosRankSum < -20.0;' undefined variable ReadPosRankSum 
 
 
-source("lib/run_pseudoref.R")
-library(farmeR)
-inputdf <- data.frame(input.vcf="largedata/gatk_vcf/JRIAL1A_joint_call.filtered_snps.vcf",
-                      out.fa="largedata/pgenome/mysample.fa")
-run_pseudoref(inputdf,
-              ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
-              gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
-              email="yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemh", 8))
---
+++ profiling/4.Variant_call/4.B.9_GATK_bam_backup.R
@@ -0,0 +1,17 @@
+### Jinliang Yang
+### June 7th, 2016
+
+
+library(farmeR)
+
+files <- list.files(path="largedata/gatk_vcf", pattern="RG.bam$")
+
+# /iplant/home/yangjl/methyl_teo20/bam_dedup
+run_iput(files, jobs1cpu=18, localpwd="largedata/gatk_vcf")
+
+###>>> In this path: cd /home/jolyang/Documents/Github/methylation
+###>>> RUN: sbatch -p bigmemh --mem 8196 --ntasks=1 /home/jolyang/Documents/Github/methylation/slurm-script/run_iput_array.sh
+
+
+
+
