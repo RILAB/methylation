7e8e4ed4b7e45ca18cb5f2efe63ac2bdf0766bdf
Jinliang Yang <yangjl0930@gmail.com>
2016-06-27 15:42:52 -0700

    updates

--- profiling/0.SRA_data/0.B.3_extractor.R
+++ profiling/0.SRA_data/0.B.3_extractor.R
@@ -1,3 +1,11 @@
+### Jinliang
+### June 27th, 2016
+
+library(data.table)
+meth <- fread("largedata/bismark/SRR850332_pe.CX_report.txt")
+
+
+
 
 #(III) Running the Bismark bismark_methylation_extractor
 bismark_methylation_extractor -p --comprehensive --no_overlap --multicore 8 --buffer_size 50%  /group/jrigrp4/BS_teo20/WGBS/BSM/SRR1610959_pe.bam -o /home/jolyang/Documents/Github/methylation/largedata/maizebs/
