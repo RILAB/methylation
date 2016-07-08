0f5c62598c39a23440eccc175954d536b3b8b260
Jinliang Yang <yangjl0930@gmail.com>
2016-07-08 15:18:19 -0700

    updates

--- profiling/2.popgen/2.C.1_angsd_GL.R
+++ profiling/2.popgen/2.C.1_angsd_GL.R
@@ -15,7 +15,7 @@ run_ANGSD(type = "GL", shfile = "slurm-script/run_angsd_gl.sh",
           email = "yangjl0930@gmail.com",
           runinfo = c(TRUE, "bigmemh", 16))
 
-#ngsF --n_ind 20 --n_sites 122960346 --glf teo20.glf.gz --out teo20 --approx_EM --fast_lkl
+#ngsF --n_ind 20 --n_sites 10000000 --glf teo20.glf --out teo20 --approx_EM --fast_lkl
 
 ### do theta
 run_ANGSD(type = "theta", shfile = "slurm-script/run_angsd_theta.sh",
@@ -24,4 +24,4 @@ run_ANGSD(type = "theta", shfile = "slurm-script/run_angsd_theta.sh",
           anc = "$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
           nInd=20, minInd=5,
           email = "yangjl0930@gmail.com",
-          runinfo = c(TRUE, "bigmemm", 32))
+          runinfo = c(TRUE, "bigmemh", 16))
