a668537ace7b05ffa35b1ba4914dd89cefacdf06
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-07-13 23:54:52 -0700

    updates

--- profiling/4.Variant_call/4.C.2_BCFtools_pseduoref.R
+++ profiling/4.Variant_call/4.C.2_BCFtools_pseduoref.R
@@ -4,7 +4,7 @@
 
 library(farmeR)
 
-###
+### test
 "samtools faidx $HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa 1:1-10000 | bcftools consensus -i JRI20_joint_call.filtered_snps.vcf.gz -o out1.fa"
 "samtools faidx $HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa 1:1-10000 | bcftools consensus JRI20_joint_call.filtered_snps.vcf.gz -o out2.fa"
 "samtools faidx $HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa 1:1-10000 > out0.fa"
@@ -12,10 +12,12 @@ library(farmeR)
 "bcftools consensus -i -s NA001 -f in.fa in.vcf.gz > out.fa"
 
 
-run_pseudoref(inputdf,
-              ref.fa="$HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa",
-              gatkpwd="$HOME/bin/GenomeAnalysisTK-3.5/GenomeAnalysisTK.jar",
-              email="yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemh", 8))
+cmd <- "bcftools consensus -i -f $HOME/dbcenter/AGP/AGPv2/Zea_mays.AGPv2.14.dna.toplevel.fa largedata/gatk_vcf/JRI20_joint_call.filtered_indels.vcf.gz -o largedata/pgenome/teo20.fa"
+
+set_farm_job(slurmsh = "slurm-script/bcf2cons.sh",
+             shcode = cmd, wd = NULL, jobid = "cns",
+             email = "yangjl0930@gmail.com", runinfo = c(TRUE, "bigmemm", "8"))
+
 
 
 
